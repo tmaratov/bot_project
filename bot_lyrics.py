cc7463c0e189e672cbc1254e3355c2a3189cb13e
Маратов Тилек <t.maratov@capital.kg>
2023-03-23 15:09:01 +0600

    vse

--- bot_lyrics.py
+++ bot_lyrics.py
@@ -1,20 +1,102 @@
 import telebot
+from telebot import types
+
+from time import sleep
+from lyricsgenius import Genius
+import textwrap
+
 
 token = '5979685133:AAE1ciAqk800SNOsjtd91udbVdzFeQjWtz4'
+g = Genius("HcAgpKN4LvM3-BkegBAT2qYW_nPiiQ2PEAv5Nt4KU4l2CrwET91NrfiyhUbIkg2z")\
+
 
 bot = telebot.TeleBot(token)
 
-
-
-
 @bot.message_handler(commands=['start'])
-def welcome_message(message):
-    welcome_message = 'Работает?'
-    bot.send_message(message.chat.id, welcome_message)
+def start(message):
+    bot.send_message(message.chat.id, 'Hi! It is a Genius.com! \n To find your lyurics of your favourite music type "/search" ')
+
+@bot.message_handler(commands=['search'])
+# def search(message):
+#     def search_lyrics(message):
+#         msg1 = bot.send_message(message.chat.id, 'Name of song: ')
+#         bot.register_next_step_handler(msg1, answer)
+        
+#     msg = bot.send_message(message.chat.id, 'Name of artist: ')
+#     bot.register_next_step_handler(msg, search_lyrics)
+
+
+
+def search(message):
+    def search_lyrics(message):
+        # Save the user's response to the "Name of artist" prompt
+        artist_name = message.text
+
+        def process_song(message, artist_name):
+            def get_lyrics(artist_name, song_name):
+                song = g.search_song(song_name, artist_name)
+                if song is not None:
+                    return song.lyrics
+                else:
+                    return None
+            song_name = message.text
+            lyrics = get_lyrics(artist_name, song_name)
+            if lyrics:
+                answer(message, artist_name, lyrics)
+            else:
+                bot.send_message(message.chat.id, f"Sorry, I could not find lyrics for {song_name} by {artist_name}.")
+
+        msg1 = bot.send_message(message.chat.id, 'Name of song: ')
+        bot.register_next_step_handler(msg1, lambda message: process_song(message, artist_name))
 
 
 
 
+    msg = bot.send_message(message.chat.id, 'Name of artist: ')
+    bot.register_next_step_handler(msg, search_lyrics)
+
+def answer(message, artist_name, lyrics):
+    MAX_MESSAGE_LENGTH = 4000
+    if len(lyrics) > MAX_MESSAGE_LENGTH:
+        # Split lyrics into smaller chunks
+        chunks = [lyrics[i:i+MAX_MESSAGE_LENGTH] for i in range(0, len(lyrics), MAX_MESSAGE_LENGTH)]
+        for i, chunk in enumerate(chunks):
+            bot.send_message(message.chat.id, f"Here are the lyrics for {message.text} by {artist_name} (part {i+1}/{len(chunks)}):\n\n{chunk}")
+    else:
+        bot.send_message(message.chat.id, f"Here are the lyrics for {message.text} by {artist_name}:\n\n{lyrics}")
+
+
+        
+
+    
+
+    # artist = g.search_artist(artist_name, max_songs=1)
+    # if artist is None:
+    #     bot.send_message(message.chat.id, 'Artist not found.')
+    #     return
+    # song = g.search_song(message.text, artist.name)
+    # if song is None:
+    #     bot.send_message(message.chat.id, 'Song not found.')
+    #     return
+    
+    # max_length = 4096
+    # while len(song.lyrics) > 0:
+    #     chunk = song.lyrics[:max_length]
+    #     song.lyrics = song.lyrics[max_length:]
+    #     bot.send_message(message.chat.id, chunk)
+    
+
+
+@bot.message_handler(content_type=['text'])
+def text(message):
+    bot.send_message(message.chat.id, 'You want something? ')
+
+
+
+
+
+
+    
 
 
 bot.infinity_polling()
--- lyrics.py
+++ lyrics.py
@@ -1,53 +1,72 @@
-from genius import Genius
+# from genius import Genius
+import lyricsgenius
 import requests
 import json
+from bot_lyrics import search, search_lyrics
 
-g = Genius(access_token="HcAgpKN4LvM3-BkegBAT2qYW_nPiiQ2PEAv5Nt4KU4l2CrwET91NrfiyhUbIkg2z")
 
-artist = 'Kendrick Lamar' #input('>> name of artist ')
-song = 'LOVE' #input('>> name of song ')
-token = 'YmyXNq15z-RRysYsaAIdnaIuJLmrWTy_KRHXpXl1UrS1MYvDVof9oKoEZuN2V4We'
+def song():
+    g = lyricsgenius.Genius("HcAgpKN4LvM3-BkegBAT2qYW_nPiiQ2PEAv5Nt4KU4l2CrwET91NrfiyhUbIkg2z")
 
-artist = g.search_artist(artist)
-url = 'https://api.genius.com/search?='
-headers = {'Authorization': "Bearer " + token}
-data = {'q': song}
-response = requests.get(url, headers=headers, params=data)
+    # artist = input('>> name of artist ')
+    # song = input('>> name of song ')
+    token = 'YmyXNq15z-RRysYsaAIdnaIuJLmrWTy_KRHXpXl1UrS1MYvDVof9oKoEZuN2V4We'
 
-if response.status_code == 200:
-    data = response.json()
+    artist = g.search_artist(search, max_songs=1)
+
+    songs = artist.song(search_lyrics)
+    return songs.lyrics
+
+# def print_song():
+
+
+
+
+
+
+
+
+
+
+# url = 'https://api.genius.com/search?='
+# headers = {'Authorization': "Bearer " + token}
+# data = {'q': song}
+# response = requests.get(url, headers=headers, params=data)
+
+# if response.status_code == 200:
+#     data = response.json()
 
     
-    for hit in data["response"]["hits"]:
-        if hit["result"]["primary_artist"]["name"] == artist:
-            song_info = hit
-            break
-        print(song_info)
-            # if hit["result"]["title"] == song:
-            #     print(hit["result"]["id"])
-            #     id_of_song = hit["result"]["id"]
+#     for hit in data["response"]["hits"]:
+#         if hit["result"]["primary_artist"]["name"] == artist:
+#             song_info = hit
+#             break
+#         print(song_info)
+#             # if hit["result"]["title"] == song:
+#             #     print(hit["result"]["id"])
+#             #     id_of_song = hit["result"]["id"]
 
 
 
 
 
 
-# for songs in artist.songs_by_popularity:
-    # print(songs)
-    # songs = songs.split('(')
-    # print(songs)
-    # if song == songs[0]:
-    #     print(song)
+# # for songs in artist.songs_by_popularity:
+#     # print(songs)
+#     # songs = songs.split('(')
+#     # print(songs)
+#     # if song == songs[0]:
+#     #     print(song)
 
 
 
-song = g.get_song(song_id=id_of_song)
-print(song.title_with_featured)
-print(song.release_date_for_display)
+# song = g.get_song(song_id=id_of_song)
+# print(song.title_with_featured)
+# print(song.release_date_for_display)
 
-#print(song.album)
-#for featured in song.features:
-#    print(featured.name)
+# #print(song.album)
+# #for featured in song.features:
+# #    print(featured.name)
 
-# lyrics = song.lyrics
-# print('\n'.join(lyrics))
+# # lyrics = song.lyrics
+# # print('\n'.join(lyrics))
